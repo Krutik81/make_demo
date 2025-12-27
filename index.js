fc5b4e020952b6e45e5dfa9a05281d0b5c8226c2
dharmesh d <dharmeshdev01@gmail.com>
2025-12-27 09:57:17 +0530

    git change

--- index.js
+++ index.js
@@ -10,6 +10,11 @@ app.get('/', (req, res) => {
   res.send('Hi, this is working!');
 });
 
+app.get('/demo', (req, res) => {
+  res.send('Hi, this is working!');
+});
+
+
 // Start the server using the port from the .env file
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
